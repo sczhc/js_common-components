a25ea908dc99a4b7691bf3b961d8b435447637d5
jason <jason@wizmacau.com>
2020-04-07 18:56:09 +0800

    upload

--- UploadTailoring/index.js
+++ UploadTailoring/index.js
@@ -1,12 +1,28 @@
 function Tailoring() {
     if (typeof document !== 'undefined') {
         var args = arguments[0];
-        if(args.el) {
-            
+        if (args.el) {
+            var $input = document.querySelector('input[name="upload"]');
+            var $button = document.querySelector('.upload-button');
+            var $img = document.querySelector('img[name="update-img"]');
+            $button.addEventListener('click', function () {
+                $input.click();
+            })
+            $input.addEventListener('change', function (event) {
+                var files = event.target.files[0]
+                var reader = new FileReader()
+                reader.onload = function () {
+                    $img.src = reader.result
+                }
+                reader.readAsDataURL(files);
+            })
+            if (args.proportion) {
+
+            }
         } else {
-            return new Error('请传入el 参数')
+            throw new Error('请传入el 参数')
         }
     } else {
-        return new Error('没有doeument对象')
+        throw new Error('没有doeument对象')
     }
 }
